d6c0bdd520161cc56099d940c912d9f90f1685ca
Binod Marikhu <bnod_468@hotmail.com>
2017-08-23 22:19:36 +0545

    HelloWorld Component and its navigation route added

--- CoreNg2/ClientApp/app/app.module.shared.ts
+++ CoreNg2/ClientApp/app/app.module.shared.ts
@@ -9,6 +9,7 @@ import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { HelloWorldComponent } from './components/helloworld/helloworld.component'
 
 @NgModule({
     declarations: [
@@ -16,7 +17,8 @@ import { CounterComponent } from './components/counter/counter.component';
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
-        HomeComponent
+        HomeComponent,
+        HelloWorldComponent
     ],
     imports: [
         CommonModule,
@@ -27,6 +29,7 @@ import { CounterComponent } from './components/counter/counter.component';
             { path: 'home', component: HomeComponent },
             { path: 'counter', component: CounterComponent },
             { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'hello', component: HelloWorldComponent },
             { path: '**', redirectTo: 'home' }
         ])
     ]
